19d6c0f5e8f49102a28a32b496b2d36dc5bcf64c
ginobvhc <ginobvhc@gmail.com>
2020-12-21 09:59:52 -0300

    first commit

---
+++ links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/emacs/doom/config.el ~/.doom.d/config.el
+ln -sf ~/dotfiles/emacs/doom/custom.el ~/.doom.d/custom.el
+ln -sf ~/dotfiles/emacs/doom/packages.el ~/.doom.d/packages.el
+
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
